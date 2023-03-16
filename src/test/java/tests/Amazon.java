bf08536ed7af1a6e2c365d7705262e5fcc89f115
Medina1988 <aimedenjuzganova@gmail.com>
2023-03-16 11:06:14 -0400

    delete my method

--- src/test/java/tests/Amazon.java
+++ src/test/java/tests/Amazon.java
@@ -4,7 +4,5 @@ public class Amazon {
     public static void main(String[] args) {
         System.out.println("Welcome to Amazon!");
     }
-    public void welcome(){
-        System.out.println("Welcome our city");
-    }
+    
 }
