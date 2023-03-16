ea09c41d36faf386f6528294a4589c07a00666af
Medina1988 <aimedenjuzganova@gmail.com>
2023-03-16 11:05:04 -0400

    Welcome to city

--- src/test/java/tests/Amazon.java
+++ src/test/java/tests/Amazon.java
@@ -4,4 +4,7 @@ public class Amazon {
     public static void main(String[] args) {
         System.out.println("Welcome to Amazon!");
     }
+    public void welcome(){
+        System.out.println("Welcome our city");
+    }
 }
